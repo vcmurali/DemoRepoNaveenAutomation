c7cadafae2be9e03519dcadb183413bb8eab8f8b
vcmurali <vcmurali@hotmail.com>
2023-07-23 16:25:36 -0500

    commit

---
+++ src/main/java/com/qa/libs/TestBase.java
@@ -0,0 +1,9 @@
+package com.qa.libs;
+
+public class TestBase {
+	
+	public void testBase() {
+		System.out.println("TestBase program");
+	}
+
+}
---
+++ src/test/java/com/qa/tests/TestProgram.java
@@ -0,0 +1,12 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
+public class TestProgram {
+
+	@Test
+	
+	public void testProgram() {
+		System.out.println("Test program");
+	}
+}
